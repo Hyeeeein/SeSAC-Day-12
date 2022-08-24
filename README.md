17335a7895ec5e67fdff55c698be38172ed0c0ee
hyein <khi_9866@naver.com>
2022-08-24 22:24:02 +0900

    새싹 12일차

---
+++ README.md
@@ -0,0 +1,8 @@
+# SeSAC-Day-12
+
+### 새싹 12일차
++ white-space
++ word-break
++ filter
++ 반응형 웹페이지 nature 만들기
++ js 데이터 타입
---
+++ js1.js
@@ -0,0 +1,27 @@
+let user = 50;
+let userTelnumber;
+let userAddress;
+
+let num = 50;
+let str = '자바스크립트';
+let num1 = '2022';
+let value1 = 2022 + '2023'
+console.log(typeof value1);
+
+let str1 = '2023';
+let str2 = '안녕!' + str1 + '\n년';
+console.log(str2)
+/* 템플릿 리터럴 벡틱*/
+let str3 = `안녕하세요 올해는 ${str1} 년입니다`
+console.log(str3)
+
+// boolean : 논리값, 참 거짓
+let isOn = true;
+let isOff = false;
+console.log(isOn)
+
+// typeof : 어떤 타입의 데이터인지 확인 (string, number, boolean 등)
+console.log(typeof isOff)
+
+let js1 = 10;
+console.log(typeof js1)
